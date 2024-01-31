ecff590d5f3b5329f658f7bc43859c89c2529679
Andy Chhuon <chhuonandy@gmail.com>
2024-01-31 17:31:34 -0500

    Setup backend and app skeleton

---
+++ condo-backend/index.js
@@ -0,0 +1,12 @@
+//Node .
+const express = require("express");
+const app = express();
+const PORT = 8080;
+
+app.listen(process.env.PORT || PORT, () => {
+  console.log(`Server running on http://localhost:${PORT}`);
+});
+
+app.use(express.json());
+
+// app.post(...)
