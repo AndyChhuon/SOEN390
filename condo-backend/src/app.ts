84dceaed722bc63c75c7a52a77ac50eeea1184d1
Andy Chhuon <chhuonandy@gmail.com>
2024-02-11 12:58:11 -0500

    Add error handling

--- condo-backend/src/app.ts
+++ condo-backend/src/app.ts
@@ -5,34 +5,16 @@ const express = require("express");
 const app = express();
 const { updateUserValues, initializeUser } = require("./firebase/userService");
 const { parseJSONOrString } = require("./utils/utils");
-import { NextFunction, Request, Response } from "express";
-
-// Custom error handling middleware
-const errorHandler = (
-  err: Error,
-  req: Request,
-  res: Response,
-  next: NextFunction
-) => {
-  console.error(err.stack);
-  res.status(500).send("Something went wrong!");
-};
-
-app.use((req: Request, res: Response, next: NextFunction) => {
-  throw new Error("Something went wrong!");
-});
-
-// Use the custom error handling middleware
-app.use(errorHandler);
+import { Request, Response } from "express";
 
 app.use(express.json());
 
-app.post("/updateUserValues", (req: Request, res: Response) => {
+app.post("/updateUserValues", async (req: Request, res: Response) => {
   if (req.body.tokenId && req.body.userValues) {
     try {
       console.log("Calling updateUserValues endpoint");
       const userValues = parseJSONOrString(req.body.userValues);
-      updateUserValues(req.body.tokenId, userValues, res);
+      await updateUserValues(req.body.tokenId, userValues, res);
     } catch (e) {
       console.log(e);
       res.status(500).send("Internal server error");
@@ -42,11 +24,11 @@ app.post("/updateUserValues", (req: Request, res: Response) => {
   }
 });
 
-app.post("/initializeUser", (req: Request, res: Response) => {
+app.post("/initializeUser", async (req: Request, res: Response) => {
   if (req.body.tokenId && req.body.email) {
     try {
       console.log("Calling initializeUser endpoint");
-      initializeUser(req.body.tokenId, req.body.email, res);
+      await initializeUser(req.body.tokenId, req.body.email, res);
     } catch (e) {
       console.log(e);
       res.status(500).send("Internal server error");
--- condo-backend/src/index.ts
+++ condo-backend/src/index.ts
@@ -1,6 +1,14 @@
 const { app } = require("./app");
 const PORT = 8080;
 
+app.use((err: any, req: any, res: any, next: any) => {
+  if (res.headersSent) {
+    return next(err);
+  }
+  console.error(err);
+  res.status(500).send("Oh no!");
+});
+
 app.listen(process.env.PORT || PORT, () => {
   console.log(`Server running on http://localhost:${PORT}`);
 });
