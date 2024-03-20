e7e90f7f98407636437df718d0d6c0345815d0fe
camilavejar <camila.vejar01@gmail.com>
2024-03-20 16:40:20 -0400

    Add jest and index.test.js

---
+++ condo-management-app/test/index.test.js
@@ -0,0 +1,7 @@
+// to run tests 
+// 1. cd into condo-management-app
+// 2. npm install
+// 3. npm run test
+// There are no tests rn so if you run it shoukd just say 'No tests found, exiting with code 1'
+
+//When adding your test, include the description with a comment right before.
