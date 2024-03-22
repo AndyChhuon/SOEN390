5a3eb4945bf9a78c845e0ad6ef62a5d848eef2a9
Andy Chhuon <chhuonandy@gmail.com>
2024-03-21 21:26:40 -0400

    Added eslint for code quality

--- condo-backend/src/openai/chat.ts
+++ condo-backend/src/openai/chat.ts
@@ -1,54 +1,48 @@
-import OpenAI from "openai";
-import { Response } from "express";
-const {
-  addFinancialsToProperty,
-  getIdFromToken,
-  userExists,
-  addToPropertiesOwned,
-  getCostEntries,
-} = require("../firebase/firebase");
-const dotenv = require("dotenv");
-dotenv.config();
+import OpenAI from 'openai'
+import { type Response } from 'express'
+const { getIdFromToken, userExists } = require('../firebase/firebase')
+const dotenv = require('dotenv')
+dotenv.config()
 
 const openaiApiKey = process.env.OPENAI_API_KEY
   ? process.env.OPENAI_API_KEY
-  : "";
+  : ''
 
-const openai = new OpenAI();
+const openai = new OpenAI()
 
-openai.apiKey = openaiApiKey;
+openai.apiKey = openaiApiKey
 
-async function generateResponse(
-  tokenId: String,
+async function generateResponse (
+  tokenId: string,
   messagesArr: OpenAI.Chat.ChatCompletionMessageParam[],
   res: Response
 ) {
-  const id = await getIdFromToken(tokenId);
+  const id = await getIdFromToken(tokenId)
   if (await userExists(id)) {
     const completion = await openai.chat.completions.create({
-      model: "gpt-3.5-turbo",
+      model: 'gpt-3.5-turbo',
       messages: [
         {
-          role: "system",
+          role: 'system',
           content:
-            "You are a helpful customer service agent for a condo management company. Answer questions and if the user asks you to do something, pretend to do it.",
+            'You are a helpful customer service agent for a condo management company. Answer questions and if the user asks you to do something, pretend to do it.'
         },
-        ...messagesArr,
+        ...messagesArr
       ],
-      stream: true,
-    });
+      stream: true
+    })
 
     for await (const chunk of completion) {
-      if (chunk && chunk.choices[0].delta.content) {
-        console.log(chunk.choices[0].delta.content);
-        res.write(chunk.choices[0].delta.content);
+      if (chunk?.choices[0].delta.content) {
+        console.log(chunk.choices[0].delta.content)
+        res.write(chunk.choices[0].delta.content)
       }
     }
 
-    res.end();
+    res.end()
   } else {
-    res.status(404).send("User not found");
+    res.status(404).send('User not found')
   }
 }
 
-export { generateResponse };
+export { generateResponse }
--- condo-backend/src/utils/utils.ts
+++ condo-backend/src/utils/utils.ts
@@ -1,12 +1,12 @@
 const parseJSONOrString = (input: any) => {
-  if (typeof input === "string") {
+  if (typeof input === 'string') {
     try {
-      return JSON.parse(input); // Attempt to parse the input as JSON
+      return JSON.parse(input) // Attempt to parse the input as JSON
     } catch (error) {
-      return input; // Parsing failed, return the original string
+      return input // Parsing failed, return the original string
     }
   }
-  return input; // Return as-is if it's already a parsed object
-};
+  return input // Return as-is if it's already a parsed object
+}
 
-export { parseJSONOrString };
+export { parseJSONOrString }
