8a8433667d23b64aeb07b93a79f9d42b3e0e0ed0
Andy Chhuon <chhuonandy@gmail.com>
2024-04-07 19:28:26 -0400

    add usertype

--- condo-backend/src/firebase/userService.ts
+++ condo-backend/src/firebase/userService.ts
@@ -1,84 +1,86 @@
-import { type Response } from 'express'
+import { type Response } from "express";
 
 const {
   getIdFromToken,
   updateUserValuesDB,
   userExists,
-  getUserValues
-} = require('./firebase')
+  getUserValues,
+} = require("./firebase");
 
 const userValuesAllowedKeys = [
-  'city',
-  'firstName',
-  'lastName',
-  'phoneNumber',
-  'postalcode',
-  'stateProvince',
-  'streetAddress',
-  'userEmail',
-  'profileUrl',
-  'propertiesOwned'
-]
+  "city",
+  "firstName",
+  "lastName",
+  "phoneNumber",
+  "postalcode",
+  "stateProvince",
+  "streetAddress",
+  "userEmail",
+  "profileUrl",
+  "propertiesOwned",
+  "userType",
+];
 
 const defaultUserValues = {
-  city: '',
-  firstName: '',
-  lastName: '',
-  phoneNumber: '',
-  postalcode: '',
-  stateProvince: '',
-  streetAddress: '',
-  userEmail: '',
-  profileUrl: ''
-}
+  city: "",
+  firstName: "",
+  lastName: "",
+  phoneNumber: "",
+  postalcode: "",
+  stateProvince: "",
+  streetAddress: "",
+  userEmail: "",
+  profileUrl: "",
+  userType: "renter",
+};
 
 const userValueKeysAreValid = (userValues: Object) => {
-  const keys = Object.keys(userValues)
-  return keys.every((key) => userValuesAllowedKeys.includes(key))
-}
+  const keys = Object.keys(userValues);
+  return keys.every((key) => userValuesAllowedKeys.includes(key));
+};
 
 const updateUserValues = async (
   tokenId: string,
   userValues: Object,
   response: Response
 ) => {
-  const id = await getIdFromToken(tokenId)
+  const id = await getIdFromToken(tokenId);
   if (!userValueKeysAreValid(userValues)) {
-    response.status(400).send('Invalid user values')
+    response.status(400).send("Invalid user values");
   } else if (await userExists(id)) {
     // user exists and userValues are valid, update userValues
-    updateUserValuesDB(id, userValues)
+    updateUserValuesDB(id, userValues);
     response.status(200).send({
-      message: 'User values updated successfully',
-      userValues
-    })
+      message: "User values updated successfully",
+      userValues,
+    });
   } else {
-    response.status(404).send('User not found')
+    response.status(404).send("User not found");
   }
-}
+};
 
 const initializeUser = async (
   tokenId: string,
   email: string,
   response: Response
 ) => {
-  const id = await getIdFromToken(tokenId)
+  const id = await getIdFromToken(tokenId);
   if (await userExists(id)) {
     response.status(200).send({
-      message: 'User logged in successfully',
-      userValues: await getUserValues(id)
-    })
+      message: "User logged in successfully",
+      userValues: await getUserValues(id),
+    });
   } else {
     const userValues = {
       ...defaultUserValues,
-      userEmail: email
-    }
-    updateUserValuesDB(id, userValues)
+      userEmail: email,
+    };
+    updateUserValuesDB(id, userValues);
     response.status(200).send({
-      message: 'User signed up successfully',
-      userValues
-    })
+      message: "User signed up successfully",
+      userValues,
+    });
   }
-}
+};
 
-export { updateUserValues, initializeUser }
+export { updateUserValues, initializeUser };
